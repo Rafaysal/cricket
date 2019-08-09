3da0a84ba2da35efeb8d109f42fef11b1c63f7c5
Rafaysal <rafaysal@hotmail.com>
2019-08-08 19:46:48 -0700

    Updates

---
+++ precache-manifest.665aa39cb7c4e158a57c8827f2d581fc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569071449c1f838daa8f873f05f6f248",
+    "url": "/cricket/index.html"
+  },
+  {
+    "revision": "e8868e879429a56ffe70",
+    "url": "/cricket/static/css/main.51447fe2.chunk.css"
+  },
+  {
+    "revision": "2a767f6e47a46a879ece",
+    "url": "/cricket/static/js/2.a7bedd98.chunk.js"
+  },
+  {
+    "revision": "e8868e879429a56ffe70",
+    "url": "/cricket/static/js/main.227f9191.chunk.js"
+  },
+  {
+    "revision": "8a7157d55b193ca451b1",
+    "url": "/cricket/static/js/runtime~main.37bf4802.js"
+  },
+  {
+    "revision": "9cdeb1b33c5668361a77d693f4e50674",
+    "url": "/cricket/static/media/bground.9cdeb1b3.jpeg"
+  },
+  {
+    "revision": "f13b473c55053e78009e690c67c26581",
+    "url": "/cricket/static/media/buzz.f13b473c.mp3"
+  },
+  {
+    "revision": "8126d6e3e958d757d314e5757f328154",
+    "url": "/cricket/static/media/player.8126d6e3.png"
+  },
+  {
+    "revision": "be92b2d493e2e76138fc43fbc780cb12",
+    "url": "/cricket/static/media/player2.be92b2d4.png"
+  },
+  {
+    "revision": "4d18a54fc1b1b8158b3b2ba6474f7c01",
+    "url": "/cricket/static/media/q1.4d18a54f.jpg"
+  },
+  {
+    "revision": "63637ce9658e3a6a06a01021ef68fd65",
+    "url": "/cricket/static/media/tick.63637ce9.mp3"
+  },
+  {
+    "revision": "35ff188a07be3aac9c165626ece2fdb7",
+    "url": "/cricket/static/media/win.35ff188a.mp3"
+  },
+  {
+    "revision": "82e3eeae933e8c40bc6bd7ea1f6de11b",
+    "url": "/cricket/static/media/yawn.82e3eeae.png"
+  }
+]);
